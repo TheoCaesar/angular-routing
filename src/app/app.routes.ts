1cd03995f0cde82d164a756484165d220b72609a
TheoCaesar <theocaesar7@gmail.com>
2024-08-02 04:05:59 +0100

    rendering routes with router outlets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Route } from "@angular/router";
 import { TaskComponent } from "./tasks/task/task.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
 export const routes:Route[] = [
+    {
+        path: '',
+        component:NoTaskComponent
+    },
     {
         path:'tasks',          //<web-domain>/tasks
         component: TaskComponent
