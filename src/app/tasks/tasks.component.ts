9eda0cbb89d2921672381a4e79611b74b09d2f42
TheoCaesar <theocaesar7@gmail.com>
2024-08-05 21:10:27 +0100

    extract params with observables (activatedRoute) & apply sorting

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, input, computed } from '@angular/core';
+import { Component, inject, input, computed, signal, DestroyRef } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { Params, RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -14,13 +14,28 @@ import { Params, RouterLink } from '@angular/router';
 })
 export class TasksComponent {
   userId = input.required<string>()
-  orderBy = input<'asc'|'desc'>();
+  // orderBy = input<'asc'|'desc'>();
+  orderBy = signal<'asc'|'desc'|undefined>(undefined);
   sortParam: Params|null|undefined = {orderBy: this.orderBy() === 'asc' ? 'desc' : 'asc'};
   private taskService = inject(TasksService)
+  private activatedRoute = inject(ActivatedRoute)
+  private destroyRef = inject(DestroyRef)
+  
   userTasks = computed(()=>this.taskService.allTasks()
-    .filter(task => task.userId == this.userId()));
-  ngOnInit() {
-
-    console.log("hello", this.userTasks)
+    .filter(task => task.userId == this.userId())
+    .sort((a, b)=>{
+      if (this.orderBy() == 'asc') {
+        return a.id > b.id ? -1 : 1;
+      }
+      else return a.id > b.id ? 1 : -1;
+    })
+  );
+  
+    ngOnInit() {
+    const queryParamsSubscr = this.activatedRoute.queryParams.subscribe((params)=>{
+      this.orderBy.set(params['orderBy']);
+    })
+    this.destroyRef.onDestroy(()=>queryParamsSubscr.unsubscribe());
+    console.log("hello", this.orderBy())
   }
 }
