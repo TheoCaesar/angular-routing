e9aec46779229c025389b475175bca8f107d810b
TheoCaesar <theocaesar7@gmail.com>
2024-08-06 03:59:35 +0100

    router guards - canMatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Route } from "@angular/router";
+import { CanMatch, CanMatchFn, RedirectCommand, Route, Router } from "@angular/router";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolverUserNameFunction, resolveSelectedUserTitle, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
@@ -6,7 +6,16 @@ import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { userRoutes } from "./users/user.routes";
+import { inject } from "@angular/core";
 
+const dummyCanMatch: CanMatchFn = (route,segments)=>{
+    const router = inject(Router)
+    const getAccess = Math.random();
+    // return (getAccess > 0.5) ? true : false
+    return (getAccess > 0.5) ? true : new RedirectCommand(
+        router.parseUrl('/unauthorized')
+    );
+}
 export const routes:Route[] = [
     {
         path: '',
@@ -17,6 +26,7 @@ export const routes:Route[] = [
         path:'user/:userId',         
         component: UserTasksComponent,
         children:userRoutes,
+        canMatch:[dummyCanMatch],
         data: {
             message: 'hello...'
         }, 
